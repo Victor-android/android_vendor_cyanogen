8b222ab9c1becbe1ac559d53794dccf18852c66f
genokolar <genokolar@gmail.com>
2011-04-17 01:57:50 -0700

    add gen2/gen1

--- products/cyanogen_blade.mk
+++ products/cyanogen_blade.mk
@@ -15,24 +15,39 @@ PRODUCT_BRAND := zte
 PRODUCT_DEVICE := blade
 PRODUCT_MODEL := Blade
 PRODUCT_MANUFACTURER := ZTE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cyanogen_blade BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+TARGET_BLADE_BOARD_NAME := gen2
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cyanogen_blade BUILD_ID=FRF91 BUILD_DISPLAY_ID=CM7-2.3-Geno-Goapk BUILD_FINGERPRINT=google/passion/passion:2.3.3/Geno/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 Geno 102588 release-keys"
 
 #
 # Set ro.modversion
 #
+ifeq ($(TARGET_BLADE_BOARD_NAME),gen2)
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Blade
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Blade-gen2
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-Blade
+            ro.modversion=CyanogenMod-7.0.1-Blade-gen2
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-Blade-KANG
+            ro.modversion=CyanogenMod-7.0.1-Blade-Geno-gen2
     endif
 endif
-
+else
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Blade-gen1
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.1-Blade-gen1
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.1-Blade-Geno-gen1
+    endif
+endif
+endif
 #
 # Copy legend specific prebuilt files
 #
